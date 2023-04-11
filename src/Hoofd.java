ce1e9ed66e50f596393c4169af58a5b68d050abf
Alwan Rashid <Pietje123>
2023-04-11 16:23:32 +0200

    verandering

--- src/Hoofd.java
+++ src/Hoofd.java
@@ -7,7 +7,7 @@ import java.util.Collections;
 
 public class Hoofd {
 	public static void main(String[] args) {
-		
+KAPOT		
 		System.out.println("How many players?");
 		Scanner scan = new Scanner(System.in);
 
